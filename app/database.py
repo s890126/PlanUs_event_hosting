0bc67469cfef1022b287be0194f4314e555dc9f4
s890126 <ss26214555@gmail.com>
2024-07-13 13:36:15 +0800

    Modified database connection

--- app/database.py
+++ app/database.py
@@ -4,7 +4,13 @@ from sqlalchemy.orm import sessionmaker
 from .config import settings
 import os
 
-SQLALCHEMY_DATABASE_URL = f"postgresql://{os.getenv('DATABASE_USERNAME')}:{os.getenv('DATABASE_PASSWORD')}@{os.getenv('DATABASE_HOSTNAME')}:{os.getenv('DATABASE_PORT')}/{os.getenv('DATABASE_NAME')}"
+SQLALCHEMY_DATABASE_URL = (
+    f"postgresql://{os.getenv('DATABASE_USERNAME')}:"
+    f"{os.getenv('DATABASE_PASSWORD')}@"
+    f"{os.getenv('DATABASE_HOSTNAME')}:"
+    f"{os.getenv('DATABASE_PORT')}/"
+    f"{os.getenv('DATABASE_NAME')}?sslmode=require"
+)
 #SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
 #SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:ss890126@localhost/planus'
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
