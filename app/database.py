4b1df77cfde4916d026a90a20d3ae5b50d359d46
s890126 <ss26214555@gmail.com>
2024-07-17 09:55:04 +0800

    Edit picture size

--- app/database.py
+++ app/database.py
@@ -4,25 +4,25 @@ from sqlalchemy.orm import sessionmaker
 from .config import settings
 import os
 
-# Ensure all required environment variables are set
-required_env_vars = [
-    'DATABASE_USERNAME', 'DATABASE_PASSWORD', 'DATABASE_HOSTNAME',
-    'DATABASE_PORT', 'DATABASE_NAME'
-]
-for var in required_env_vars:
-    if var not in os.environ:
-        raise EnvironmentError(f"Environment variable {var} is not set")
+# # Ensure all required environment variables are set
+# required_env_vars = [
+#     'DATABASE_USERNAME', 'DATABASE_PASSWORD', 'DATABASE_HOSTNAME',
+#     'DATABASE_PORT', 'DATABASE_NAME'
+# ]
+# for var in required_env_vars:
+#     if var not in os.environ:
+#         raise EnvironmentError(f"Environment variable {var} is not set")
 
-# Construct the database URL
-SQLALCHEMY_DATABASE_URL = (
-    f"postgresql://{os.getenv('DATABASE_USERNAME')}:"
-    f"{os.getenv('DATABASE_PASSWORD')}@"
-    f"{os.getenv('DATABASE_HOSTNAME')}:"
-    f"{os.getenv('DATABASE_PORT')}/"
-    f"{os.getenv('DATABASE_NAME')}?sslmode=require"
-)
+# # Construct the database URL
+# SQLALCHEMY_DATABASE_URL = (
+#     f"postgresql://{os.getenv('DATABASE_USERNAME')}:"
+#     f"{os.getenv('DATABASE_PASSWORD')}@"
+#     f"{os.getenv('DATABASE_HOSTNAME')}:"
+#     f"{os.getenv('DATABASE_PORT')}/"
+#     f"{os.getenv('DATABASE_NAME')}?sslmode=require"
+# )
 #SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
-#SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:ss890126@localhost/planus'
+SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:ss890126@localhost/planus'
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit = False, autoflush = False, bind = engine)
